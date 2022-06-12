356431903571327e11c04e8f1552ad2638695814
syifa-119140116 <syifa.119140116@student.itera.ac.id>
2022-06-13 01:01:16 +0700

    Update profil user

--- app/src/main/java/capstone/project/tahun/kedua/daurnusantara/AkunActivity.kt
+++ app/src/main/java/capstone/project/tahun/kedua/daurnusantara/AkunActivity.kt
@@ -1,30 +1,48 @@
 package capstone.project.tahun.kedua.daurnusantara
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import capstone.project.tahun.kedua.danus.databinding.ActivityAkunBinding
-import com.bumptech.glide.Glide
+import capstone.project.tahun.kedua.daurnusantara.databinding.ActivityAkunBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class AkunActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAkunBinding
+    private lateinit var preferenceSession: PreferenceSession
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAkunBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        showLoading(true)
 
-        val data = intent.getParcelableExtra<ListStory>(OBJECT) as DataKerajinan
-
-        with(binding) {
-            Glide.with(this@AkunActivity)
-                .load(foto.photoUrl)
-                .into(picture)
-            nama.text = data.name
-            email.text = data.email
-        }
         showLoading(false)
+
+        tampil()
+
+        binding.keluar.setOnClickListener {
+            logoutNow()
+            finish()
+        }
+    }
+
+    private fun tampil(){
+        val user = Firebase.auth.currentUser
+        user?.let {
+            val emaile = user.email
+            binding.email.text = emaile
+        }
+    }
+
+    private fun logoutNow() {
+        preferenceSession = PreferenceSession(this)
+        preferenceSession.sesiDelete()
+        Log.d(".HomeActivity", "Periksa : ${preferenceSession.sesiGet()}")
+        val toLogin = Intent(this@AkunActivity, LoginActivity::class.java)
+        startActivity(toLogin)
+        finish()
     }
 
     private fun showLoading(isLoading: Boolean) {
